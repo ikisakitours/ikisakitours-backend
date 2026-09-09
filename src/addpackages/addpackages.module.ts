3867868a6978ef5e12fd02d73111dbf44797f3c6
shehan-wijekoon <shehan.wijekoon@gmail.com>
2026-09-09 11:25:51 +0530

    update addpackage.module to update uploadsmodule

--- src/addpackages/addpackages.module.ts
+++ src/addpackages/addpackages.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AddpackagesController } from './addpackages.controller';
 import { AddpackagesService } from './addpackages.service';
+import { UploadsModule } from '../uploads/uploads.module';
 
 @Module({
+  imports: [UploadsModule],
   controllers: [AddpackagesController],
   providers: [AddpackagesService]
 })
